23439e5427b59425c409e11738af914c6b022984
Kyle Krattiger <mr.music25@gmail.com>
2019-02-05 18:04:42 -0800

    Switched login method to OAuth, improved progress output, added handling of backend errors

--- delete_dups_from_playlists.py
+++ delete_dups_from_playlists.py
@@ -1,40 +1,61 @@
 #!/usr/bin/env python
 from gmusicapi import Mobileclient
 import sys
+import time
 
-
-# TODO remove this unused function, previously used to return tracks from a given playlist
-def get_playlist_tracks(name, playlists):
-    for playlist in playlists:
-        if playlist['name'].lower() == name.lower():
-            tracks = playlist['tracks']
-            return tracks
-    print "ERROR: No playlist '" + name + "'found"
-    exit(1)
+### Functions
 
 def find_and_remove_dups(api, tracks):
     track_set = set()
     for track in tracks:
+        tries = 0
         trackId = track['trackId']
         entryId = track['id']
         if trackId in track_set:
-            print "    found duplicate with trackId: " + trackId + ", deleting"
-            api.remove_entries_from_playlist(entryId)
+            print "    found duplicate with trackId: " + trackId
+            while tries < 3:
+                try:
+                    sys.stdout.write("      attempting to delete... ") # Makes it so no newline appears, cleaner output
+                    api.remove_entries_from_playlist(entryId)
+                    #break
+                except:
+                    tries += 1
+                    sys.stdout.write(str(tries) + "... ")
+                    if tries == 3:
+                        print "Failed too many times, exiting!"
+                        api.logout()
+                        exit(1)
+                    time.sleep(1)
+                print "Success!"
+                break
+            time.sleep(1) # Helps prevent Google from creating backend errors
         else:
             track_set.add(trackId)
 
+### Main
 
+# Display help and exit if arguments present            
 if len(sys.argv) != 1:
     print "USAGE:"
     print "./delete_dups_from_playlists.py"
     print
-    print "     Will delete all duplicate songs within each playlist"
+    print "     Will delete all duplicate songs within each playlist" 
     exit(0)
 
+# Setup the gmusicapi
 api = Mobileclient()
-logged_in = api.login('username', 'password', Mobileclient.FROM_MAC_ADDRESS)
+api.__init__()
 
-if logged_in:
+
+# Check to see if OAuth credentials available, ask user to setup if not
+try:
+    api.oauth_login(Mobileclient.FROM_MAC_ADDRESS)
+except:
+    print "No OAuth credentials found! Please setup in the following screen!"
+    api.perform_oauth()
+
+# Then, move on to doing all the work
+if api.is_authenticated():
     print "Successfully logged in. Finding duplicates in playlists"
     playlists = api.get_all_user_playlist_contents()
 
@@ -42,3 +63,9 @@ if logged_in:
         print "Deleting duplicates from " + playlist['name'] + "..."
         tracks = playlist['tracks']
         find_and_remove_dups(api, tracks)
+    api.logout()
+else:
+    print "Not logged in! Exiting..."
+    exit(1)
+
+print "Script has finished successfully!"
